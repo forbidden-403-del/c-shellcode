f6f1e64790d1b8433ee621a72a0e292b388fef3a
admin <admin>
2025-11-26 19:34:12 +0400

    Enhance build scripts: add MinGW-w64 directory to .gitignore and improve compile.bat messaging

--- compile.bat
+++ compile.bat
@@ -1,31 +1,43 @@
 @echo off
 setlocal enabledelayedexpansion
 
-@REM REM === Target directory ===
-@REM set MINGW_DIR=mingw64
-@REM set INSTALL_SCRIPT=install_mingw.bat
-@REM call "%INSTALL_SCRIPT%" || (
-@REM         echo Install failed.
-@REM         exit /b 1
-@REM     )
+REM ============================================
+REM  Configuration
+REM ============================================
+set "MINGW_DIR=mingw64"
+
+REM Prepend MinGW to PATH (only for this script)
 set "PATH=%CD%\%MINGW_DIR%\bin;%PATH%"
 
+
+REM ============================================
+REM  Compile section
+REM ============================================
 :compile
-REM ============================
-REM Compile with gcc (all args passed to this .bat)
-REM Example:
-REM   build.bat main.c -o main.exe
-REM ============================
+
 if "%~1"=="" (
-    echo No files/arguments passed to gcc. Skipping compilation.
+    echo [!] No input files or arguments passed to GCC.
+    echo Usage: build.bat file.c [options]
     goto :end
 )
 
-gcc -m32 -nostdlib -fno-asynchronous-unwind-tables -fno-unwind-tables -fno-exceptions -Wl,-T script.ld -o raw.exe %*
+mkdir bin 2>nul
+
+gcc -m32 ^
+    -nostdlib ^
+    -fno-asynchronous-unwind-tables ^
+    -fno-unwind-tables ^
+    -fno-exceptions ^
+    -Wl,-T script.ld ^
+    -o bin\raw.exe ^
+    %*
+
 if errorlevel 1 (
-    echo GCC compilation failed.
+    echo [!] GCC compilation failed.
     goto :end
 )
-objdump -x raw.exe
+
+echo [+] Dumping PE header info...
+objcopy -j .text -O binary bin\raw.exe bin\out.bin
 :end
 endlocal
